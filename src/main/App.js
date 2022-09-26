be1852f6776785d061c39669b3769d473f8a6552
esmyrnio <smirnios.10@gmail.com>
2022-09-26 14:30:38 +0300

    try inf

--- src/main/App.js
+++ src/main/App.js
@@ -6,20 +6,12 @@ import { GlobalStyle } from "../styles/Styles.js";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <GlobalStyle />
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Model />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <GlobalStyle />
+      <Navbar />
+      <Model />
+      <About />
+    </div>
   );
 }
 
--- src/main/Navbar.js
+++ src/main/Navbar.js
@@ -3,14 +3,9 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/">
-        {" "}
-        <h1>quickStar</h1>
-      </Link>
-      <div className="links">
-        <Link to="/">App</Link>
-        <Link to="/about">About</Link>
-      </div>
+      {" "}
+      <h1>quickStar</h1>
+      <div className="links"></div>
     </nav>
   );
 };
