5e8e3fdb3b05a7ef57754f21ee69fdc066529562
esmyrnio <smirnios.10@gmail.com>
2022-09-26 14:08:07 +0300

    router 3

--- src/main/App.js
+++ src/main/App.js
@@ -4,13 +4,23 @@ import About from "./About.js";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { GlobalStyle } from "../styles/Styles.js";
 
-class App extends React.Component {
-  static propTypes = {
-    children: PropTypes.node,
-  };
-  render() {
-    const { children } = this.props;
-    return <div>{children}</div>;
-  }
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <GlobalStyle />
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Model />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
+
 export default App;
