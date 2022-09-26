b19a40411ab960f8600bb9f5bdb7990de53f4c19
esmyrnio <smirnios.10@gmail.com>
2022-09-26 16:50:17 +0300

    r

--- src/main/App.js
+++ src/main/App.js
@@ -1,12 +1,26 @@
-import React from "react";
-import PropTypes from "prop-types";
-class App extends React.Component {
-  static propTypes = {
-    children: PropTypes.node,
-  };
-  render() {
-    const { children } = this.props;
-    return <div>{children}</div>;
-  }
+import Navbar from "./Navbar.js";
+import Model from "./Model.js";
+import About from "./About.js";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { GlobalStyle } from "../styles/Styles.js";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <GlobalStyle />
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Model />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
+
 export default App;
