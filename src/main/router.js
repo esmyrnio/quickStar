4acbba9a73e23e76dfccb638d0d1045408a843e2
esmyrnio <smirnios.10@gmail.com>
2022-09-26 16:46:02 +0300

    www

--- src/main/router.js
+++ src/main/router.js
@@ -2,11 +2,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default (
   <Router history={hashHistory}>
-    <Switch>
-      <Route path="/" component={App}>
-        <IndexRoute component={Model} />
-        <Route path="about" component={About} />
-      </Route>
-    </Switch>
+    <Route path="/" component={App}>
+      <IndexRoute component={Model} />
+      <Route path="about" component={About} />
+    </Route>
   </Router>
 );
